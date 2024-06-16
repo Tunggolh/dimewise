cc8cbcf3f0c11a2a6fc721219c85577d10040fb3
Marlo Tunggolh <marlotunggolh@gmail.com>
2024-06-16 12:51:32 +0800

    Add account type enum

---
+++ src/application/enum/AccountTypeEnum.ts
@@ -0,0 +1,9 @@
+export enum AccountTypeEnum {
+  CASH = 'Cash',
+  DEBIT_CARD = 'Debit Card',
+  CREDIT_CARD = 'Credit Card',
+  SAVINGS = 'Savings',
+  INVESTMENT = 'Investment',
+  LOAN = 'Loan',
+  OTHER = 'Other',
+}
