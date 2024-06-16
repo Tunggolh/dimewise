3eb0498c0df207ec3a3081a69cc6aca0347764dc
Marlo Tunggolh <marlotunggolh@gmail.com>
2024-06-16 13:33:53 +0800

    Update create account dto

--- src/infrastructure/http/dto/accounts/create-account.dto.ts
+++ src/infrastructure/http/dto/accounts/create-account.dto.ts
@@ -1,8 +1,17 @@
+import { IsEnum, IsNumber, IsOptional, IsString } from 'class-validator';
 import { AccountTypeEnum } from 'src/application/enum/AccountTypeEnum';
 
 export class CreateAccountDto {
-  name: string;
-  description: string;
-  balance: number;
-  type: AccountTypeEnum;
+  @IsString()
+  readonly name: string;
+
+  @IsString()
+  @IsOptional()
+  readonly description: string;
+
+  @IsNumber()
+  readonly balance: number;
+
+  @IsEnum(AccountTypeEnum)
+  readonly type: AccountTypeEnum;
 }
